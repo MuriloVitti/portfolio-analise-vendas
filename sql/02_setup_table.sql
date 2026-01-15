51d8aa7c9c3d5cae7435c13a2313fc6fd519e0ec
MURILO  MAFFEI VITTI <murilo.mvitti@gmail.com>
2026-01-15 16:23:08 -0300

    Delete sql/02_setup_table.sql

--- sql/02_setup_table.sql
+++
@@ -1,8 +0,0 @@
--- Pergunta 1: Quais são as categorias com o maior desconto médio?
-SELECT 
-    category, 
-    ROUND(AVG(discount_percentage), 2) AS media_desconto
-FROM amazon_reviews
-GROUP BY category
-ORDER BY media_desconto DESC
-LIMIT 5;
